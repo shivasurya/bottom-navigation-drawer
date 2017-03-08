7c41110e5859c1b4581390663ba7720bec87c147
shivasurya <s.shivasurya@gmail.com>
2017-03-08 22:28:55 +0530

    added fragments for bottom navigation

--- app/src/main/java/com/ivisionblog/apps/bottomnavigationdrawer/MainActivity.java
+++ app/src/main/java/com/ivisionblog/apps/bottomnavigationdrawer/MainActivity.java
@@ -1,15 +1,30 @@
 package com.ivisionblog.apps.bottomnavigationdrawer;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import com.ivisionblog.apps.bottomnavigationdrawer.fragments.ButtonFragment;
+import com.ivisionblog.apps.bottomnavigationdrawer.fragments.HomeFragment;
+import com.ivisionblog.apps.bottomnavigationdrawer.fragments.ItemFragment;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        changeFragment(new HomeFragment());
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +33,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    changeFragment(new HomeFragment());
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    changeFragment(new ButtonFragment());
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    changeFragment(new ItemFragment());
                     return true;
             }
             return false;
@@ -32,14 +47,12 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    private void changeFragment(Fragment fm){
+        android.support.v4.app.FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content, fm);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        ft.addToBackStack(null);
+        ft.commit();
     }
 
 }
---
+++ app/src/main/java/com/ivisionblog/apps/bottomnavigationdrawer/fragments/ButtonFragment.java
@@ -0,0 +1,51 @@
+package com.ivisionblog.apps.bottomnavigationdrawer.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ivisionblog.apps.bottomnavigationdrawer.R;
+import com.google.android.gms.plus.PlusOneButton;
+
+/**
+ * A fragment with a Google +1 button.
+ */
+public class ButtonFragment extends Fragment {
+
+    // The request code must be 0 or greater.
+    private static final int PLUS_ONE_REQUEST_CODE = 0;
+    // The URL to +1.  Must be a valid URL.
+    private final String PLUS_ONE_URL = "http://www.i-visionblog.com";
+    private PlusOneButton mPlusOneButton;
+
+
+    public ButtonFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_button, container, false);
+
+        //Find the +1 button
+        mPlusOneButton = (PlusOneButton) view.findViewById(R.id.plus_one_button);
+
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Refresh the state of the +1 button each time the activity receives focus.
+        mPlusOneButton.initialize(PLUS_ONE_URL, PLUS_ONE_REQUEST_CODE);
+    }
+
+
+}
